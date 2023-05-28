1477707104798e763ac5ea49a36a4e25351f7002
Tapwatero <charliewoolf20@gmail.com>
2023-05-28 21:32:18 +0100

    idk

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './input.css';
 import "./output.css";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Vote from "./Vote";
 import Analytics from "./components/Analytics";
+import Vote from "./Vote";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
